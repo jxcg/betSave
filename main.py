185eca2d111e0fdc4ac9b45d106181a97ef7f856
jxcg <154761377+jxcg@users.noreply.github.com>
2025-01-15 23:18:39 +0000

    New calculator engine + changed format

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ import os
 import yaml
 import datetime
 
+
+
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-app = Flask(__name__)
-app.secret_key = os.urandom(24)
+
+app             = Flask(__name__)
+app.secret_key  = os.urandom(24)
+
+def sanitise_floats(value, d=0.00):
+    try:
+        return float(value)
+    except (TypeError, ValueError):
+        return d
+
 
 def read_config():
     with open("config.yaml", "r") as f:
@@ -19,35 +29,38 @@ def read_config():
 
 config = read_config()
 
+# google blueprint object
 google_blueprint = make_google_blueprint(
-    client_id=config['secrets']['client_id'],
-    client_secret=config['secrets']['client_secret'],
-    scope=["openid", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"],
-    redirect_url='http://localhost:5001/'
+
+    client_id       = config['secrets']['client_id'],
+    client_secret   = config['secrets']['client_secret'],
+    scope           = ["openid", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"],
+    redirect_url    = 'http://localhost:5001/'
+
 )
 
 app.register_blueprint(google_blueprint, url_prefix="/login")
 # SQLite "sqlite:///bets.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = config['database']['uri']
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"]           = config['database']['uri']
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]    = False
+db                                              = SQLAlchemy(app)
 
 
 class Bet(db.Model):
     __tablename__ = "bets"
     id = db.Column(db.Integer, primary_key=True)
-    back_stake = db.Column(db.Float, default=0.00)
-    lay_stake = db.Column(db.Float, default=0.00)
-    back_odds = db.Column(db.Float, default=0.00)
-    lay_odds = db.Column(db.Float, default=0.00)
-    user_email = db.Column(db.String(255), nullable=False)
-    bet_date = db.Column(db.String, nullable=False)
-    bet_type = db.Column(db.String(255), default="Qualifying")
-    profit_loss_bookie = db.Column(db.Float, default=0.00)
-    profit_loss_exchange = db.Column(db.Float, default=0.00)
-    event = db.Column(db.String(255))
-    exchange = db.Column(db.String(255))
-    bookie = db.Column(db.String(255))
+    back_stake              = db.Column(db.Float, default=0.00)
+    lay_stake               = db.Column(db.Float, default=0.00)
+    back_odds               = db.Column(db.Float, default=0.00)
+    lay_odds                = db.Column(db.Float, default=0.00)
+    user_email              = db.Column(db.String(255), nullable=False)
+    bet_date                = db.Column(db.String, nullable=False)
+    bet_type                = db.Column(db.String(255), default="Qualifying")
+    profit_loss_bookie      = db.Column(db.Float, default=0.00)
+    profit_loss_exchange    = db.Column(db.Float, default=0.00)
+    event                   = db.Column(db.String(255))
+    exchange                = db.Column(db.String(255))
+    bookie                  = db.Column(db.String(255))
 
 
 
@@ -130,6 +143,7 @@ def delete_bet(bet_id):
 
     db.session.delete(bet)
     db.session.commit()
+
     return redirect(url_for("tracker_page"))
 
 
@@ -137,23 +151,16 @@ def delete_bet(bet_id):
 def edit_bet(bet_id):
     print("editing:", bet_id)
     user_email = get_user_email(google.authorized)
-    if not user_email:
-        return redirect(url_for("google.login"))
+
+    if not user_email: return redirect(url_for("google.login"))
+    
     bet = Bet.query.get_or_404(bet_id)
-    if bet.user_email != user_email:
-        return "Unauthorized", 403
+    if bet.user_email != user_email: return "Unauthorized", 403
 
     db.session.delete(bet)
     db.session.commit()
     return redirect(url_for("tracker_page"))
 
-"""
-@app.route('/bet_form', methods=["POST"])
-def handle_bet_form():
-    action = request.form.get("action")
-    if action == "add_bet":
-        return redirect(url_for)
-"""    
 
 @app.route("/tracker", methods=["GET", "POST"])
 def tracker_page():
@@ -164,26 +171,36 @@ def tracker_page():
     
     if request.method == "POST":
         # extract form data
-        bet_date = request.form.get("bet_date","").strip()
-        bet_type = request.form.get("bet_type","Qualifying")
-        bookie = request.form.get("bookie_name")
-        exchange = request.form.get("exchange_name")
-        event = request.form.get("event_name")
-        back_odds = request.form.get("back_odds")
-        lay_odds = request.form.get("lay_odds")
-        #calculate back_stake = request.form.get("back_stake")
-        #calc lay_stake = request.form.get("lay_stake")   
+        bet_date        = request.form.get("bet_date","").strip()
+        bet_type        = request.form.get("bet_type","Qualifying")
+        bookie          = request.form.get("bookie_name")
+        exchange        = request.form.get("exchange_name")
+        event           = request.form.get("event_name")
+        back_odds       = request.form.get("back_odds")
+        lay_odds        = request.form.get("lay_odds")
+        back_stake      = request.form.get("back_stake")
+
+        if not back_odds:
+            back_odds   = sanitise_floats(back_odds)
+
+        if not lay_odds:
+            lay_odds    = sanitise_floats(lay_odds)
+
         if not bet_date:
-            bet_date = str(datetime.date.today())
+            bet_date    = str(datetime.date.today())
+        
+        ## CALCULATE LAY STAKE automatically?
 
         user_bet = Bet(
-            user_email=user_email,
-            bet_date=bet_date,
-            bet_type=bet_type,
-            bookie=bookie,
-            exchange=exchange,
-            back_odds=back_odds,
-            event=event
+            user_email  = user_email,
+            back_stake  = back_stake,
+            lay_odds    = lay_odds,
+            bet_date    = bet_date,
+            bet_type    = bet_type,
+            bookie      = bookie,
+            exchange    = exchange,
+            back_odds   = back_odds,
+            event       = event
         )
 
         db.session.add(user_bet)
@@ -191,21 +208,22 @@ def tracker_page():
         print("bet added to db")
         return redirect(url_for("tracker_page"))
 
+
     """
     Query to get all bets from DB for the user
     """
 
-    all_user_bets = Bet.query.filter_by(user_email=user_email).all()
-
-    total_cumulative_profit = 0.0
+    all_user_bets               = Bet.query.filter_by(user_email=user_email).all()
+    total_cumulative_profit     = 0.0
+    
     for bet in all_user_bets:
         total_cumulative_profit += (bet.profit_loss_exchange + bet.profit_loss_bookie)
 
     return render_template(
         "tracker.html",
-        bets=all_user_bets,
-        total = total_cumulative_profit,
-        user_email = user_email
+        bets        = all_user_bets,
+        total       = total_cumulative_profit,
+        user_email  = user_email
     )
 
 
@@ -216,6 +234,6 @@ def handle_bet_form():
 
 if __name__ == "__main__":
     with app.app_context():
-        db.drop_all() # debug
+        #db.drop_all() # debug
         db.create_all()
     app.run(host='0.0.0.0', port=5001, debug=True)
--- src/engine.py
+++ src/engine.py
@@ -1,55 +1,102 @@
-def submission_check(back_comms, back_odds_decimal, back_stake_input, lay_comms, lay_odds_decimal):
-    if not back_comms:
-        back_comms = 0
-    if not lay_comms:
-        lay_comms = 0
-    if not back_odds_decimal or not back_stake_input or not lay_odds_decimal:
-        return False
-    return True
-
 class Bet:
-    def __init__(self, back_stake, back_odds, lay_commission, back_commission, lay_odds, bet_type):
-        self.back_stake = back_stake
-        self.back_odds = back_odds
-        self.lay_commission = lay_commission
-        self.back_commission = back_commission
-        self.lay_odds = lay_odds
-        self.bet_type = bet_type
+    def __init__(self, back_stake, back_odds, back_commission, lay_commission, lay_odds, free):
+        self.free               = free, # TRUE / FALSE
+        self.back_stake         = back_stake, # BACK BET is INTERCHANGEABLE to QUALIFYING BET OR FREE BET
+        self.back_odds          = back_odds,
+        self.back_commission    = back_commission,
+        self.lay_odds           = lay_odds,
+        self.lay_commission     = lay_commission
+        pass
 
-    def get_lay_stake(self):
-        if self.bet_type == "Qualifying Bet":
-            return round((self.back_stake * self.back_odds) / self.lay_odds, 2)
-        return round(self.back_stake * (self.back_odds - 1) / self.lay_odds, 2)
 
-    def get_lay_liability(self):
-        return round(self.get_lay_stake() * (self.lay_odds - 1), 2)
+    """
+    Qualifying bet and welcome bet formulas
+    """
 
-    def get_case_back_bet_win(self):
-        back_winnings = self.back_stake * (self.back_odds - 1)
-        back_winnings -= back_winnings * self.back_commission
-        if self.bet_type == "Free Bet":
-            return round(back_winnings - self.get_lay_liability(), 2)
-        profit = round(back_winnings - self.get_lay_liability(), 2)
-        return profit
+    def get_lay_stake(self, free, stake_returned=False):
+        if not free:
+            return (
+                self.back_odds * self.back_stake / (self.lay_odds - self.lay_commission)
+                )
+        
+        if free and not stake_returned:
+            ## Free Bet, SNR
+            return (
+                (self.back_odds - 1) / (self.lay_odds - self.lay_commission) * self.back_stake # free bet in this case
+            )
 
-    def get_case_lay_bet_wins(self):
-        lay_winnings = self.get_lay_stake()
-        lay_commission_deduction = self.lay_commission * (self.get_lay_stake() * (self.lay_odds - 1))
-        if self.bet_type == "Free Bet":
-            return round(lay_winnings - lay_commission_deduction, 2)
-        profit = round(lay_winnings - lay_commission_deduction - self.back_stake, 2)
-        return profit
+        if free and stake_returned:
+            ## Free Bet, SR
+            return (
+                (self.back_odds * self.back_stake) / (self.lay_odds - self.lay_commission)
+            )   
 
-def calculate(bet_type, back_stake, back_odds, lay_commission, back_commission, lay_odds):
-    if submission_check(back_commission, back_odds, back_stake, lay_commission, lay_odds):
-        bet = Bet(back_stake, back_odds, lay_commission, back_commission, lay_odds, bet_type)
-        results = {
-            "Lay Stake": bet.get_lay_stake(),
-            "Lay Liability": bet.get_lay_liability(),
-            "Profit if Back Wins": bet.get_case_back_bet_win(),
-            "Profit if Lay Wins": bet.get_case_lay_bet_wins()
-        }
-        return results
-    else:
-        raise ValueError("Invalid input: Please ensure all required inputs are provided.")
+
+
+
+    def profit_if_back_bet_wins(self):
+        return (
+            self.back_stake * (self.back_odds - 1) - self.get_lay_stake(False) * (self.lay_odds - 1)
+            )
+
+    
+
+    def profit_if_lay_bet_wins(self, free):
+        if not free:
+            return (
+                self.get_lay_stake(False) * (1 - self.lay_commission) - self.back_stake
+                )
+
+        return (
+            self.get_lay_stake(True) * (1 - self.lay_commission)
+        )
+
+
+
+    def final_profit_for_qualifying_bet(self):
+        return self.profit_if_lay_bet_wins(False)
+
+
+    """
+    Free bet - SNR
+    """
+
+    def profit_free_bet_wins(self, stake_returned=False):
+        if not stake_returned:
+            return (
+                (self.back_odds - 1) * self.back_stake - (self.lay_odds - 1) * self.get_lay_stake(True)
+            )
+        
+        ## IF STAKE RETURNED
+        
+        return (
+            self.back_stake * self.back_odds - (self.lay_odds - 1) * self.get_lay_stake(True, True) # Free + SR
+        )
+
+
+    
+    def final_profit_free_bet(self, stake_returned):
+        if not stake_returned:
+            return (
+                self.get_lay_stake(True) * (1 - self.lay_commission)
+            )
+        
+        # IF FREE + STAKE_RETURNED
+        return (
+            self.back_stake
+        )
+
+    
+
+    
+
+
+
+
+
+
+
+    """
+    Free bet - SR
+    """
 
